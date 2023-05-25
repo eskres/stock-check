5899589641be4067a5e8d2c418830eab744fe92b
eskres <einar.skreslett@gmail.com>
2023-05-25 22:53:06 +0100

    fixed tests

--- stockCheck.test.js
+++ stockCheck.test.js
@@ -8,11 +8,12 @@ test ('solution resolves or rejects correctly', () => {
     const e = solution(['9999-1111-2222-3333-4444', '8888-1111-2222-3333']);
     const f = solution(['8888-1111-2222-3333', '8888-2222-3333-4444']);
     const g = solution([]);
-    expect(a).toEqual(['invalid-format', 'fail']);
-    expect(b).toEqual('9999-1111-2222-3333', 'internal-server-error');
-    expect(c).toEqual(['8888-1111-2222-3333']);
-    expect(d).toEqual('9999-1111-2222-3333', 'internal-server-error');
-    expect(e).toEqual(['invalid-format', '9999-1111-2222-3333-4444']);
-    expect(f).toEqual(['8888-1111-2222-3333', '8888-2222-3333-4444']);
-    expect(g).toEqual(['internal_system_error'])
+
+    expect(a).rejects.toEqual(['invalid-format', 'fail']);
+    expect(b).rejects.toEqual(['9999-1111-2222-3333', 'internal-server-error']);
+    expect(c).resolves.toEqual(['8888-1111-2222-3333']);
+    expect(d).rejects.toEqual(['9999-1111-2222-3333', 'internal-server-error']);
+    expect(e).rejects.toEqual(['invalid-format', '9999-1111-2222-3333-4444']);
+    expect(f).resolves.toEqual(['8888-1111-2222-3333', '8888-2222-3333-4444']);
+    expect(g).rejects.toEqual(['internal_system_error']);
 });
